2f1731f79e0945a34922227196942db45996bca4
Tom Greenwood <tomgreenwood10@gmail.com>
2020-11-20 16:56:50 +0000

    reformat to 80 char line length

--- PSPNet/builder.py
+++ PSPNet/builder.py
@@ -2,7 +2,8 @@
 File containing class for biulding pyramid scene parsing network (PSPNet).
 """
 
-from keras.layers import AveragePooling2D, Conv2D, BatchNormalization, Activation, UpSampling2D, Input, Concatenate
+from keras.layers import AveragePooling2D, Conv2D, BatchNormalization, \
+    Activation, UpSampling2D, Input, Concatenate
 from keras.models import Model
 
 from BaseModel import SegmentationModel, get_pool_size
@@ -56,46 +57,59 @@ class PSPNet(SegmentationModel):
             x = res_block(x, filters, self.activations, f'resblock_{i + 1}',
                           self.block_types, self.n_convs, self.conv_in_skip)
 
-            # Assign an aux output if there is room and it isn't the same at the main output
-            # Up sample to match input shape
-            if i >= self.aux_output_block and aux_output is False and i != self.blocks - 1:
+            # Assign an aux output if there is room and it isn't the same at 
+            # the main output. Up sample to match input shape
+            if (i >= self.aux_output_block 
+                and aux_output is False 
+                and i != self.blocks - 1):
                 x = Conv2D(1, (1, 1), name='conv_aux_output')(x)
                 x = BatchNormalization(name='bn_aux_output')(x)
-                upsample_scalar = int(self.input_shape[-2] / x._keras_shape[-2])
-                x = UpSampling2D((upsample_scalar, upsample_scalar), interpolation='bilinear', name='aux_upsample')(x)
-                aux_output = Activation(self.output_activations, name='AUX_OUTPUT')(x)
+                upsample_scalar = int(self.input_shape[-2] / 
+                                      x._keras_shape[-2])
+                x = UpSampling2D((upsample_scalar, upsample_scalar), 
+                    interpolation='bilinear', name='aux_upsample')(x)
+                aux_output = Activation(self.output_activations, 
+                    name='AUX_OUTPUT')(x)
 
         x = pyramid_module(x)
         upsample_scalar = int(self.input_shape[-2] / x._keras_shape[-2])
-        # Conv with 5 filters for 5 classes (in practice set) - change as required
+        # Conv with 5 filters for 5 classes (in practice set) - change as req.
         x = Conv2D(5, (1, 1), name='conv_main_output')(x)
         x = BatchNormalization(name='bn_main_output')(x)
-        x = UpSampling2D((upsample_scalar, upsample_scalar), interpolation='bilinear', name='main_upsample')(x)
-        main_output = Activation(self.output_activations, name='MAIN_OUTPUT')(x)
-        outputs = [aux_output, main_output] if aux_output is not False else main_output
+        x = UpSampling2D((upsample_scalar, upsample_scalar),
+            interpolation='bilinear', name='main_upsample')(x)
+        main_output = Activation(self.output_activations,
+            name='MAIN_OUTPUT')(x)
+        outputs = [aux_output, main_output] if aux_output else main_output
         self.net = Model(inputs, outputs, name='ResNet')
 
 
-def pyramid_branch(inputs, pooled_size, activation=None, batch_normalisation=False, n_branches=4):
+def pyramid_branch(inputs, pooled_size, activation=None, 
+                   batch_normalisation=False, n_branches=4):
     if inputs.shape[1] != inputs.shape[2]:
-        raise ValueError(f"Inputs should be square but found to have shape {inputs.shape}")
+        raise ValueError("Inputs should be square but found to have shape " +
+                         f"{inputs.shape}")
 
     pool_dim = int(inputs.shape[1]) / pooled_size
     pool_dim = int(pool_dim)
     pool_size = (pool_dim, pool_dim)
     name = f'{pooled_size}x{pooled_size}_pyramid_branch'
 
-    x = AveragePooling2D(pool_size=pool_size, padding='same', name='pool_' + name)(inputs)
-    x = Conv2D(int(int(x.shape[-1]) / n_branches), (1, 1), padding='same', name='conv_' + name)(x)
+    x = AveragePooling2D(pool_size=pool_size, padding='same',
+        name='pool_' + name)(inputs)
+    x = Conv2D(int(int(x.shape[-1]) / n_branches), (1, 1), padding='same', 
+        name='conv_' + name)(x)
     if batch_normalisation:
         x = BatchNormalization(name='bn_' + name)(x)
     if activation:
         x = Activation(activation, name=activation + '_' + name)(x)
-    x = UpSampling2D((pool_dim, pool_dim), interpolation='bilinear', name='upsamp_' + name)(x)
+    x = UpSampling2D((pool_dim, pool_dim), interpolation='bilinear',
+        name='upsamp_' + name)(x)
     return x
 
 
-def pyramid_module(inputs, activation=None, batch_normalisation=False, pool_sizes=(1, 2, 5, 10)):
+def pyramid_module(inputs, activation=None, batch_normalisation=False, 
+                   pool_sizes=(1, 2, 5, 10)):
     pyramid_outputs = []
     for pool_size in pool_sizes:
         pyramid_outputs.append(pyramid_branch(inputs,
